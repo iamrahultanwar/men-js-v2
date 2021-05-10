a9529befd5ac509ddaff9345ef1d08f970e9aee9
iamrahultanwar <mr.rahultanwar@gmail.com>
2021-05-10 22:07:29 +0530

    removed react , added edge

--- providers/admin/index.js
+++ providers/admin/index.js
@@ -1,12 +1,16 @@
-const express = require("express");
 const path = require("path");
-
+const edge = require("edge.js").default;
 module.exports = (ctx) => {
   const Locations = ctx.container.Locations;
   const Server = ctx.container.Server;
   const rootDir = Locations.get("helper").rootPath;
-  Server.use(express.static(path.join(rootDir, "public")));
-  Server.get("/admin", function (req, res) {
-    res.sendFile(path.join(rootDir, "public", "index.html"));
+  edge.mount(path.join(rootDir, "views"));
+
+  Server.get("/admin", async (req, res) => {
+    const html = await edge.render("index", {
+      greeting: "Hello world",
+    });
+
+    return res.send(html);
   });
 };
